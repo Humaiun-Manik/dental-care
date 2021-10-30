f24d2389e7ceeac678ab785c60fc2ca1d6fbade6
Humaiun-Manik <humaiunkobirr@gmail.com>
2021-10-30 23:46:33 +0600

    update firebase config

--- src/Pages/Login/Firebase/firebse.config.js
+++ src/Pages/Login/Firebase/firebse.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC97K9OqssQeZdmRrww-h4g9o_7GhRQT70",
-    authDomain: "dental-clinic-authentication.firebaseapp.com",
-    projectId: "dental-clinic-authentication",
-    storageBucket: "dental-clinic-authentication.appspot.com",
-    messagingSenderId: "830958819706",
-    appId: "1:830958819706:web:922279ad796cfad7fe5ec3"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 export default firebaseConfig;
